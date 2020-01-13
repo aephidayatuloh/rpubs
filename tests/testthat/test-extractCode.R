eec1ad5953423142717ddd33e7f12acedfd1ff0c
Aep Hidayatuloh <aef.stk@gmail.com>
2020-01-13 12:12:33 +0700

    Update testthat

--- tests/testthat/test-extractCode.R
+++ tests/testthat/test-extractCode.R
@@ -1,3 +1,3 @@
 test_that("Length of vector result", {
-  expect_equal(length(extractCode(url = "http://rpubs.com/aephidayatuloh/sendgmail")), 6)
+  expect_equal(length(extractCode(url = "http://rpubs.com/aephidayatuloh/sendgmail")), 1)
 })
