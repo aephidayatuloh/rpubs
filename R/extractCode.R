92bbd02667b5cb1e3dab891a8c0ea8fcd66c5805
Aep Hidyatuloh <aef.stk@gmail.com>
2020-01-26 05:10:53 +0700

    Defunct exractCode()

--- R/extractCode.R
+++ R/extractCode.R
@@ -9,25 +9,26 @@
 #'  be removed in the next version of this package. Use \code{rpubs_code()} instead.
 #' }
 #'
-#' @name extractCode-deprecated
+#' @name extractCode-defunct
 #' @param url Character. URL of RPubs article, e.g. url = "http://rpubs.com/aephidayatuloh/sendgmail".
-#' @param files Character. File name for the extracted code as R script, e.g. \code{code.R}.
+#' @param path Character. File name for the extracted code as R script, e.g. \code{code.R}.
 #' @param output Logical. Should extraction include output of the code? Default to \code{FALSE}, means only R script will be extracted.
+#' @param ... Other parameter for defunct.
 #' @import rvest
 #' @importFrom xml2 read_html
 #' @return vector
-#' @details If \code{files = NULL} then the extracted script will be print on console or as vector if you assign to an object. One code block is one element of vector.
-#' @examples
-#' extractCode(url = "http://rpubs.com/aephidayatuloh/sendgmail", file = NULL, output = FALSE)
-#'
+#' @details If \code{path = NULL} then the extracted script will be print on console or as vector if you assign to an object. One code block is one element of vector.
+#' @examples \donttest{
+#' extractCode(url = "http://rpubs.com/aephidayatuloh/sendgmail", path = NULL, output = FALSE)
+#' }
 #'
 #'
 #' @export
-extractCode <- function(url, files = NULL, output = FALSE){
+extractCode <- function(url, path = NULL, output = FALSE, ...){
   # if (as.character(match.call()[[1]]) == "extractCode") {
   #   warning("please use rpubs_code() instead of extractCode()", call. = FALSE)
   # }
-  .Deprecated(msg = "'extractCode()' will be removed in the next version\nUse 'rpubs_code()' instead")
+  .Defunct(msg = "'extractCode()' has been removed from this package\nUse 'rpubs_code()' instead.")
   pg <- read_html(url)
 
   iframe_link <- paste0("http:",
@@ -54,9 +55,9 @@ extractCode <- function(url, files = NULL, output = FALSE){
       node)
     )
 
-  if(is.null(files)){
+  if(is.null(path)){
     paste0(sprintf("# %s\n\n", url), paste(gsub("\n", "", code), collapse = "\n\n"))
   } else {
-    writeLines(text = paste0(sprintf("# %s\n\n", url), paste(gsub("\n", "", code), collapse = "\n\n")), con = files)
+    writeLines(text = paste0(sprintf("# %s\n\n", url), paste(gsub("\n", "", code), collapse = "\n\n")), con = path)
   }
 }
